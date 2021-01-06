568ac027fcc2f407e67ba15e0dc57d83bfa79323
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-05 22:20:40 -0700

    Initial construction

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week 01 Notes",
       url: "week1/index.html"
+    },
+    {
+      label: "Week 02 Notes",
+      url: "week2/index.html"
     }
   ]
+  
+  //Run function to add items to OL with ID of notes
+  window.onload = links.forEach(link => {
+
+    var newli = document.createElement("li");
+    newli.innerHTML = "<a href=" + link.url + ">" + link.label + "</a>";
+
+    document.getElementById("notes").appendChild(newli);
+
+  });
