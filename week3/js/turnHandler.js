1762fc3105dbdcf26151669076d5b353807d97e6
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-24 14:39:13 -0700

    controls

--- week3/js/turnHandler.js
+++ week3/js/turnHandler.js
@@ -7,15 +7,39 @@ function deal() {
 
     //put cards down slowly on dealer zone
     for (i = 0; i < 2; i++) {
-        let newCard = document.createElement("img")
-        newCard.src = cardDeck[newCards[i]].cardImg
-        setTimeout(() => document.getElementById("opponentArea").appendChild(newCard), 1000 * i)
+        let newCard = document.createElement("img");
+        newCard.src = cardDeck[newCards[i]].cardImg;
+        setTimeout(() => document.getElementById("opponentArea").appendChild(newCard), 1000 * i);
     }
 
     //put cards down slowly on player zone
     for (i = 2; i < 4; i++) {
-        let newCard = document.createElement("img")
-        newCard.src = cardDeck[newCards[i]].cardImg
-        setTimeout(() => document.getElementById("playerArea").appendChild(newCard), 1000 * i)
+        let newCard = document.createElement("img");
+        newCard.src = cardDeck[newCards[i]].cardImg;
+        setTimeout(() => document.getElementById("playerArea").appendChild(newCard), 1000 * i);
     }
+
+    //clear deal button, add hit and stay button
+    document.getElementById("buttonAndResult").innerHTML = '';
+    let hitButton = document.createElement("input");
+    hitButton.id = "hitButton";
+    hitButton.type = "button";
+    hitButton.value = "HIT!";
+    hitButton.onclick = playerHit;
+    let stayButton = document.createElement("input");
+    stayButton.id = "stayButton";
+    stayButton.type = "button";
+    stayButton.value = "STAY!";
+    stayButton.onclick = computerTurn;
+
+    document.getElementById("buttonAndResult").appendChild(hitButton);
+    document.getElementById("buttonAndResult").appendChild(stayButton);
+}
+
+function playerHit() {
+
+}
+
+function computerTurn() {
+
 }
