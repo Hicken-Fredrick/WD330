1394849871d9369df365cdcf831fbb783a70aa5d
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-24 14:14:29 -0700

    dealing slowly random cards

--- week3/js/turnHandler.js
+++ week3/js/turnHandler.js
@@ -1,5 +1,21 @@
-function addCard() {
-    let newCard = document.createElement("img")
-    newCard.src = cardDeck[0].cardImg
-    document.getElementById("opponentArea").appendChild(newCard)
+function deal() {
+    //first 2 for dealer second 2 for player
+    let newCards = [];
+    for (i = 0; i < 4; i++) {
+        newCards[i] = Math.floor(Math.random() * 52);
+    }
+
+    //put cards down slowly on dealer zone
+    for (i = 0; i < 2; i++) {
+        let newCard = document.createElement("img")
+        newCard.src = cardDeck[newCards[i]].cardImg
+        setTimeout(() => document.getElementById("opponentArea").appendChild(newCard), 1000 * i)
+    }
+
+    //put cards down slowly on player zone
+    for (i = 2; i < 4; i++) {
+        let newCard = document.createElement("img")
+        newCard.src = cardDeck[newCards[i]].cardImg
+        setTimeout(() => document.getElementById("playerArea").appendChild(newCard), 1000 * i)
+    }
 }
