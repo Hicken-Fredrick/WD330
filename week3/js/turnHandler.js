b9fc7853d7c6bac74fb1e23b83fdf19f58bd4cf7
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-24 17:10:50 -0700

    img fix, turn logic start, grammer

--- week3/js/turnHandler.js
+++ week3/js/turnHandler.js
@@ -1,21 +1,15 @@
-function deal() {
-    //first 2 for dealer second 2 for player
-    let newCards = [];
-    for (i = 0; i < 4; i++) {
-        newCards[i] = Math.floor(Math.random() * 52);
-    }
-
+async function deal() {
     //put cards down slowly on dealer zone
     for (i = 0; i < 2; i++) {
         let newCard = document.createElement("img");
-        newCard.src = cardDeck[newCards[i]].cardImg;
+        newCard.src = cardDeck[Math.floor(Math.random() * 52)].cardImg;
         setTimeout(() => document.getElementById("opponentArea").appendChild(newCard), 1000 * i);
     }
 
     //put cards down slowly on player zone
     for (i = 2; i < 4; i++) {
         let newCard = document.createElement("img");
-        newCard.src = cardDeck[newCards[i]].cardImg;
+        newCard.src = cardDeck[Math.floor(Math.random() * 52)].cardImg;
         setTimeout(() => document.getElementById("playerArea").appendChild(newCard), 1000 * i);
     }
 
