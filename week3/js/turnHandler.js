1629da10a2293a1f4b20cc7125cf7fc4f58d71d9
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-24 17:34:50 -0700

    Basic hit button logic

--- week3/js/turnHandler.js
+++ week3/js/turnHandler.js
@@ -1,18 +1,26 @@
-async function deal() {
-    //put cards down slowly on dealer zone
+function deal() {
+    //setup total to us for hand value
+    let total = 0;
+    //put cards down slowly on dealer zone & set dealer hand value
     for (i = 0; i < 2; i++) {
         let newCard = document.createElement("img");
-        newCard.src = cardDeck[Math.floor(Math.random() * 52)].cardImg;
+        let card = Math.floor(Math.random() * 52)
+        newCard.src = cardDeck[card].cardImg;
+        total += cardDeck[card].cardValue;
         setTimeout(() => document.getElementById("opponentArea").appendChild(newCard), 1000 * i);
     }
-
-    //put cards down slowly on player zone
+    document.getElementById("dealer").innerHTML = `Dealer - ${total}`;
+    //reset total for player hand use
+    total = 0
+    //put cards down slowly on player zone and set player hand value
     for (i = 2; i < 4; i++) {
         let newCard = document.createElement("img");
-        newCard.src = cardDeck[Math.floor(Math.random() * 52)].cardImg;
+        let card = Math.floor(Math.random() * 52)
+        newCard.src = cardDeck[card].cardImg;
+        total += cardDeck[card].cardValue;
         setTimeout(() => document.getElementById("playerArea").appendChild(newCard), 1000 * i);
     }
-
+    document.getElementById("player").innerHTML = `Player - ${total}`;
     //clear deal button, add hit and stay button
     document.getElementById("buttonAndResult").innerHTML = '';
     let hitButton = document.createElement("input");
@@ -25,13 +33,23 @@ async function deal() {
     stayButton.type = "button";
     stayButton.value = "STAY!";
     stayButton.onclick = computerTurn;
-
+    //write new buttons for play
     document.getElementById("buttonAndResult").appendChild(hitButton);
     document.getElementById("buttonAndResult").appendChild(stayButton);
 }
 
 function playerHit() {
+    //draw card
+    let card = Math.floor(Math.random() * 52)
+    //assign card
+    let newCard = document.createElement("img");
+    newCard.src = cardDeck[card].cardImg;
+    document.getElementById("playerArea").appendChild(newCard)
+    //update hand total
+    let total = document.getElementById("player").innerHTML.match(/(\d+)/);
+    total = Number(total[0]) + cardDeck[card].cardValue;
 
+    document.getElementById("player").innerHTML = `Player - ${total}`;
 }
 
 function computerTurn() {
