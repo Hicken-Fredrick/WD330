4ee7e5faa0da8f8911e3f2913a07b8cd9466d3cc
Hicken-Fredrick <Miroku101@gmail.com>
2021-01-27 20:55:52 -0700

    stuff

--- teamActivity3/ticTacToe.js
+++ teamActivity3/ticTacToe.js
@@ -1,5 +1,25 @@
 //add event listener
 document.querySelectorAll('.interactive').forEach(item => {
+    item.addEventListener('click', event => {
+        //handle click
+        if (item.innerHTML != '') {console.log("NO GO!")}
+        else {
+            let turn = document.getElementById("turn").innerHTML.match(/(\d+)/)[0];
+            let newImage = document.createElement("img");
+            if (turn == 1) {
+                newImage.src = "x.png";
+                document.getElementById("turn").innerText = "PLAYER 2 TURN";
+            }
+            if (turn == 2) {
+                newImage.src = "o.png";
+                document.getElementById("turn").innerText = "PLAYER 1 TURN";
+            }
+            item.appendChild(newImage);
+        }
+    })
+  })
+
+document.querySelectorAll('.interactive').forEach(item => {
     item.addEventListener('touchend', event => {
         //handle click
         if (item.innerHTML != '') {console.log("NO GO!")}
