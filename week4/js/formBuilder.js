8c520025fbff521c313e680875bbb0b6edf14003
Hicken-Fredrick <Miroku101@gmail.com>
2021-02-03 03:07:03 -0700

    blarg

--- week4/js/formBuilder.js
+++ week4/js/formBuilder.js
@@ -1,3 +1,4 @@
-function fillForm(this) {
-    
+function fillForm() {
+    let form = document.querySelector("form");
+    //form.elements.ownerName.value = `NO ME!`;
 }
--- week4/js/main.js
+++ week4/js/main.js
@@ -1,7 +1,5 @@
 function say() {
     let formData = getFormData("fullForm");
-    let elements = formData.elements;
-    console.log(elements);
     var object = {};
     formData.forEach((value, key) => object[key] = value);
     var json = JSON.stringify(object);
