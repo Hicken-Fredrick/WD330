6e02e4e20b480f6c20b4a856efb0b0e71eed8013
Hicken-Fredrick <Miroku101@gmail.com>
2021-02-03 02:54:52 -0700

    super basic

--- week4/js/main.js
+++ week4/js/main.js
@@ -1,7 +1,14 @@
-import {fillForm} from '/modules/formBuilder.js'
-
 function say() {
-    let stuff = document.getElementById("dataFields").childNodes;
-    console.log(stuff);
+    let formData = getFormData("fullForm");
+    let elements = formData.elements;
+    console.log(elements);
+    var object = {};
+    formData.forEach((value, key) => object[key] = value);
+    var json = JSON.stringify(object);
+    console.log(json);
   }
 
+const getFormData = (name) => {
+    const form = document.getElementById(`${name}`);
+    return new FormData(form);
+}
--- week4/js/modules/formBuilder.js
+++
@@ -1,3 +0,0 @@
-export function fillForm(this) {
-    
-}
